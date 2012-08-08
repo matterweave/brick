e07d1dee6311ff6de78339e321cb3b9d4ce29055
Dylan Sarber <dwsarber@gmail.com>
2012-08-08 00:14:25 -0700

    Add stubs for sprite creation

---
+++ sprite.lua
@@ -0,0 +1,13 @@
+-- Static sprites
+
+sprite = {}
+sprite.brick = {}
+
+sprite.new = function(image)
+    -- body
+end
+
+-- Should make all the available pentaminoes
+sprite.brick.makeBricks = function(image)
+    -- body
+end
