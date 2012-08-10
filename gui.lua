5e50eb2d7fce82e20882217e622a27dc04f80576
Juliana Arrighi <juliana.arrighi@gmail.com>
2012-08-09 23:07:33 -0700

    Added label.new and label.draw functions to gui.lua.

--- gui.lua
+++ gui.lua
@@ -4,10 +4,15 @@ label = {}
 
 -- Returns a table with information needed to draw and manipulate a text "label"
 label.new = function(text, font, x, y)
-    -- body
+    label.text = text
+    label.font = font
+    label.x = x
+    label.y = y
+    return label
 end
 
 -- Draws a label to the screen
 label.draw = function(label)
-    -- body
+	love.graphics.setFont(label.font)
+    love.graphics.print(label.text, label.x, label.y)
 end
