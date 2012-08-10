f3fb0e07108da9756a08a7610c8f89e07c8c4b8f
Dylan Sarber <dwsarber@gmail.com>
2012-08-09 22:54:55 -0700

    Add block collisions detection
    Each "brick" will have "blocks" that make it up, so the collisions detection
    will have to check each of the blocks to be accurate. Eventually, only the
    blocks directly exposed will need to be checked which will speed things up a
    bit.

--- physics.lua
+++ physics.lua
@@ -2,7 +2,22 @@
 
 physics = {}
 
--- Return true if obj1 collided with obj2, otherwise false
+-- Return true if brick1 collided with anything in blocks, otherwise return false
+-- Each "block" structure should have the typical x, y, w, h properties
+physics.checkBrickCollision = function(brick, blocks)
+    for k,v in pairs(brick.blocks) do
+        for i,b in ipairs(blocks) do
+            if v.x >= b.x and v.x <= (b.x + b.w) and
+                v.y >= b.y and v.y <= (b.y + b.h) then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+
+-- Return true if obj1 collided with obj2, otherwise return false
 physics.checkCollision = function(obj1, obj2)
     if not hasExtents(obj1) or not hasExtents(obj2) then
         return false
