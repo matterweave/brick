bfb2b67f495bf85341f0ce9e73cbc1143545d4f5
Dylan Sarber <dwsarber@gmail.com>
2012-08-11 20:43:49 -0700

    Implemented sprite.new

--- sprite.lua
+++ sprite.lua
@@ -4,8 +4,21 @@ module(..., package.seeall)
 brick = {}
 
 -- Returns a structure with info for rendering a static image
-new = function(image)
-    -- body
+new = function(image, x, y)
+    if x == nil then x = 0 end
+    if y == nil then y = 0 end
+
+    local sp = {
+        image = love.graphics.newImage(image),
+        rotation = 0,
+        x = x,
+        y = y
+    }
+
+    sp.w = sp.image.getWidth()
+    sp.h = sp.image.getHeight()
+
+    return sp
 end
 
 -- Draws the given sprite to the screen
